e439f9b8c7a803fcc76c78fa75327ca6b37c7e19
andres.ganan <andres.ganan@mantum.com.co>
2024-12-02 13:46:30 -0500

    aplicacion base

---
+++ src/main/java/com/quasar/operation/OperationApplication.java
@@ -0,0 +1,13 @@
+package com.quasar.operation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OperationApplication.class, args);
+	}
+
+}
